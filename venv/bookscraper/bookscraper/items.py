66f9da161e93b46f467342ee91b067f0503384a7
Marianac <marianackambey@gmail.com>
2024-07-12 16:00:07 +0700

    update items scrapy

--- venv/bookscraper/bookscraper/items.py
+++ venv/bookscraper/bookscraper/items.py
@@ -4,14 +4,10 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-# from scrapy.item import Item,Field
+
 
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     pass
 
-# class BookItem(Item):
-#     title = Field()
-#     price = Field()
